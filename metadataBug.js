7588497ba35479f016b2e1a49b2334a5ae490ac3
BugHunter-X <askzenai@gmail.com>
2025-01-05 19:18:06 +0100

    Add metadataBug.js file

---
+++ metadataBug.js
@@ -0,0 +1,18 @@
+```javascript
+import { MetadataRoute } from 'next/server'
+
+export default function Metadata({ params }) {
+  return (
+    <MetadataRoute>
+      {
+        title: `Product ${params.productId}`,
+        description: 'Product details',
+      }
+    </MetadataRoute>
+  );
+}
+
+export const metadata = {
+  title: 'Products',
+};
+```
